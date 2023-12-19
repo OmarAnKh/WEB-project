d293e91c00c4749ca4bcfc663bd121f19c661c18
areen abudayeh <areen.a.abodayeh@gmail.com>
2023-12-19 14:15:53 +0200

    UPD RecommendedVideo file

--- src/components/RecommendedVideo.js
+++ src/components/RecommendedVideo.js
@@ -2,24 +2,22 @@ import React from 'react';
 import { useFireStore } from "../firebase/useFirestore";
 import { Link } from 'react-router-dom';
 import "./recommendedvideo.css"
-
 function RecommendedVideo() {
     const { data } = useFireStore('RecommendedVideo');
-
     return (
         <div className="container-xxl px-4 mt-5">
             <div className="row py-5">
                 {data.map((item) => (
                     <div key={item.id} className="col-12">
                         <div className="card text-bg-dark">
-                            <img src={item.photo} className="card-img"  />
+                            <img src={item.thumbnail} className="card-img" />
                             <div className="card-img-overlay">
                                 <div className="d-flex align-items-center mb-3">
                                     <div className="avatar avatar-xxs me-2">
                                         <img
                                             className="avatar-img rounded-circle"
-                                            src={item.personalPhoto}
-                                            alt=""
+                                            src={item.photo}
+                                            alt="avatar"
                                         />
                                     </div>
                                     <h6 className="mb-0">
@@ -33,12 +31,9 @@ function RecommendedVideo() {
                                 </div>
                                 <h1 className="card-title text-white">{item.title}</h1>
                                 <p className="card-text text-white">{item.subtitle}</p>
-                                <Link
-                                    to={`/PostPageLayout/${item.index}/${item.collection_name}/${item.comments_section} `}
-                                    className="btn btn-primary">
+                                <Link to={`/PostPageLayout/${item.index}/${item.collection_name}/${item.comments_section} `} className="btn btn-primary" target="blank">
                                     <i className="bi bi-file-earmark-play pe-1"></i>{item.title2}
                                 </Link>
-                                
                             </div>
                         </div>
                     </div>
@@ -47,5 +42,4 @@ function RecommendedVideo() {
         </div>
     );
 }
-
 export default RecommendedVideo;
